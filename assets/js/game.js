874e74607c0f0e924842c533ba577b8177212845
123ojp <shepherd123ojp@gmail.com>
2018-04-25 23:10:39 +0800

    Update game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -127,7 +127,9 @@ game.States.mainMenu = function() {
         game.add.image(0, 0, "titlebg");
         var text = game.add.text(game.world.centerX, 150, "HackerRPG", menuTextStyle);
         text.anchor.setTo(0.5);
-        
+        idInput = game.add.inputField(game.world.centerX-250000, 300000, idInputStyle);		       
+        idInput.blockInput = false;		
+        idInput.focusOutOnEnter = false;
         nameInput = game.add.inputField(game.world.centerX-250, 450, nameInputStyle);
         nameInput.blockInput = false;
         nameInput.focusOutOnEnter = false;
